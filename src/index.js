acd54403c326e4d66d60c27baddef0930ab51374
David Lemus <dlemusg@eafit.edu.co>
2019-04-09 01:01:33 -0500

    bd y vistas

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const exphbs = require('express-handlebars');
+const path = require('path');
 //initialization
 const app = express();
 
 //settings
 app.set('port', process.env.PORT || 4000);
-
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs',
+  helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
 //Middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //Global variable
+app.use((req, res, next) => {
+  next();
+});
 
 //Routes
 app.use(require('./routes/index.js'))
+app.use(require('./routes/authentication.js'))
+app.use('/links', require('./routes/links.js'))
+
 //public
+app.use(express.static(path.join(__dirname, 'public')));
 
 // starting the server
 app.listen(app.get('port'), () => {
